009f9e2724115702a003261e45db60f5793e52b8
shivratangupta <shivratangupta18@gmail.com>
2020-07-14 21:47:29 +0530

    init

---
+++ src/main/java/com/codejudge/onlinejudge/CodejudgeApplication.java
@@ -0,0 +1,13 @@
+package com.codejudge.onlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodejudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodejudgeApplication.class, args);
+    }
+
+}
