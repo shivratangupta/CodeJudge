dfb31627ac1b41575ec12fb35eccd5d63979515c
shivratangupta <shivratangupta18@gmail.com>
2020-07-19 23:00:03 +0530

    add registration controller

--- src/main/java/com/codejudge/onlinejudge/controller/RegistrationController.java
+++ src/main/java/com/codejudge/onlinejudge/controller/RegistrationController.java
@@ -1,4 +1,28 @@
 package com.codejudge.onlinejudge.controller;
 
+import com.codejudge.onlinejudge.dto.ResponseDto;
+import com.codejudge.onlinejudge.dto.UserDto;
+import com.codejudge.onlinejudge.dto.UserResponseDto;
+import com.codejudge.onlinejudge.model.User;
+import com.codejudge.onlinejudge.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RegistrationController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/user/register")
+    public ResponseDto<UserResponseDto> registerUser(@RequestBody UserDto userDto) {
+
+        User user = userService.registerUser(userDto);
+        return new ResponseDto<>(
+                new UserResponseDto(user.getId(), user.getFullName(), user.getEmail(), user.isActive()),
+                HttpStatus.OK);
+    }
 }
