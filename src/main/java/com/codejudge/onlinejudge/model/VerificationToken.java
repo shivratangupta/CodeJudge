98cd1653587cb03280bb980ba65d4dd417fced67
shivratangupta <shivratangupta18@gmail.com>
2020-07-23 13:13:48 +0530

    add VerificationToken model and VerificationTokenRepository

--- src/main/java/com/codejudge/onlinejudge/model/VerificationToken.java
+++ src/main/java/com/codejudge/onlinejudge/model/VerificationToken.java
@@ -1,4 +1,62 @@
 package com.codejudge.onlinejudge.model;
 
-public class VerificationToken {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "verificationtokens")
+@Getter
+@Setter
+public class VerificationToken extends Auditable {
+
+    private static final int VALIDITY_TIME = 4 * 60;
+
+    @NotBlank
+    private String token;
+
+    @OneToOne(targetEntity = User.class)
+    private User user;
+
+    private Date expiryTime;
+
+    public VerificationToken() {
+        super();
+    }
+
+    public VerificationToken(User user) {
+
+        this.token = generateRandomUniqueToken();
+        
+        this.user = user;
+        
+        this.expiryTime = calculateExpiryTime();
+    }
+
+    private void updateToken() {
+        this.token = generateRandomUniqueToken();
+        this.expiryTime = calculateExpiryTime();
+    }
+
+    private String generateRandomUniqueToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    private Date calculateExpiryTime() {
+
+        Calendar calendar = Calendar.getInstance();
+        Date currentTimeAndDate = new Date();
+
+        calendar.setTimeInMillis(currentTimeAndDate.getTime());
+        calendar.add(Calendar.MINUTE, VALIDITY_TIME);
+
+        return calendar.getTime();
+    }
 }
--- src/main/java/com/codejudge/onlinejudge/repository/VerificationTokenRepository.java
+++ src/main/java/com/codejudge/onlinejudge/repository/VerificationTokenRepository.java
@@ -1,4 +1,11 @@
 package com.codejudge.onlinejudge.repository;
 
-public interface VerificationTokenRepository {
+import com.codejudge.onlinejudge.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+
+    VerificationToken findByToken(String token);
 }
